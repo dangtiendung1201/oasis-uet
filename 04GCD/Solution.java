1f4504b469341f56dd15e038855c5a3d8b27c466
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2023-07-22 20:47:37 +0700

    Add: GCD

---
+++ 04GCD/Solution.java
@@ -0,0 +1,22 @@
+// Import your library
+// Do not change the name of the Solution class
+public class Solution {
+    // Type your main code here
+
+    /**
+     * Calculates the greatest common divisor of two numbers.
+     */
+    public int gcd(int a, int b) {
+        if (a < 0) {
+            a = -a;
+        }
+        if (b < 0) {
+            b = -b;
+        }
+
+        if (a == 0) {
+            return b;
+        }
+        return gcd(b % a, a);
+    }
+}
