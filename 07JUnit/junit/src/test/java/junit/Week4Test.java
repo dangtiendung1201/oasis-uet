3d3b339a17f31cd5f2274746361ab6b877e03811
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2023-07-23 10:16:50 +0700

    Add: JUnit

---
+++ 07JUnit/junit/src/test/java/junit/Week4Test.java
@@ -0,0 +1,86 @@
+package junit;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Week4Test {
+    @Test
+    public void testMax2Int1() {
+        assertEquals(5, Week4.max2Int(5, 3));
+    }
+
+    @Test
+    public void testMax2Int2() {
+        assertEquals(10, Week4.max2Int(-2, 10));
+    }
+
+    @Test
+    public void testMax2Int3() {
+        assertEquals(0, Week4.max2Int(0, -1));
+    }
+
+    @Test
+    public void testMax2Int4() {
+        assertEquals(0, Week4.max2Int(0, 0));
+    }
+
+    @Test
+    public void testMax2Int5() {
+        assertEquals(3108, Week4.max2Int(1201, 3108));
+    }
+
+    @Test
+    public void testMinArray1() {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, -1, 0 };
+        assertEquals(-1, Week4.minArray(arr));
+    }
+
+    @Test
+    public void testMinArray2() {
+        int[] arr = { 1201, 3108, 12, 8, 1, 32839, 74389473, 12384 };
+        assertEquals(1, Week4.minArray(arr));
+    }
+
+    @Test
+    public void testMinArray3() {
+        int[] arr = { -2, -832, 73, 7432, 2 };
+        assertEquals(-832, Week4.minArray(arr));
+    }
+
+    @Test
+    public void testMinArray4() {
+        int[] arr = { 0, 0, 0, 0, 0 };
+        assertEquals(0, Week4.minArray(arr));
+    }
+
+    @Test
+    public void testMinArray5() {
+        int[] arr = { 3, 2, 5, 8, 53, 534, 53, 3 };
+        assertEquals(2, Week4.minArray(arr));
+    }
+
+    @Test
+    public void testCalculateBMI1() {
+        assertEquals("Thiếu cân", Week4.calculateBMI(50, 1.7));
+    }
+
+    @Test
+    public void testCalculateBMI2() {
+        assertEquals("Bình thường", Week4.calculateBMI(60, 1.7));
+    }
+
+    @Test
+    public void testCalculateBMI3() {
+        assertEquals("Thừa cân", Week4.calculateBMI(70, 1.7));
+    }
+
+    @Test
+    public void testCalculateBMI4() {
+        assertEquals("Béo phì", Week4.calculateBMI(80, 1.7));
+    }
+
+    @Test
+    public void testCalculateBMI5() {
+        assertEquals("Béo phì", Week4.calculateBMI(90, 1.5));
+    }
+}
---
+++ 07JUnit/submit/Week4.java
@@ -0,0 +1,42 @@
+// package junit;
+
+public class Week4 {
+    public static int max2Int(int a, int b) {
+        // Tim gia tri lon nhat cua 2 so nguyen
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static int minArray(int[] array) {
+        // Tim gia tri nho nhat cua 1 mang so nguyen
+        int min = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i])
+                min = array[i];
+        }
+
+        return min;
+    }
+
+    public static String calculateBMI(double weight, double height) {
+        // Tinh BMI
+        double bmi = weight / (height * height);
+        String result = "";
+
+        if (bmi < 18.5) {
+            result = "Thiếu cân";
+        } else if (bmi >= 18.5 && bmi < 23) {
+            result = "Bình thường";
+        } else if (bmi >= 23 && bmi < 25) {
+            result = "Thừa cân";
+        } else {
+            result = "Béo phì";
+        }
+
+        return result;
+    }
+}
