3040fbff1a5aaf078943d482ec6dd47309cfa9ac
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2023-07-23 11:57:07 +0700

    Fix: Transaction

--- 08Transaction/Account.java
+++ 08Transaction/Account.java
@@ -2,24 +2,41 @@ import java.util.ArrayList;
 
 public class Account {
     private double balance;
-    private ArrayList<String> transitionList = new ArrayList<String>();
+    private ArrayList<Transaction> transitionList = new ArrayList<Transaction>();
 
+    /**
+     * Print transaction.
+     */
     public void printTransaction() {
         for (int i = 0; i < transitionList.size(); i++) {
-            System.out.println("Giao dich " + (i + 1) + ": " + transitionList.get(i));
+            System.out.print("Giao dich " + (i + 1) + ": ");
+            if (transitionList.get(i).getOperation().equals(Transaction.DEPOSIT)) {
+                System.out.print("Nap tien $");
+            } else {
+                System.out.print("Rut tien $");
+            }
+            System.out.print(String.format("%.2f", transitionList.get(i).getAmount()) + ". ");
+            System.out.print("So du luc nay: $" + String.format("%.2f", transitionList.get(i).getBalance()) + ".\n");
         }
     }
 
+    /**
+     * Deposit.
+     */
     private void deposit(double amount) {
         if (amount <= 0) {
             System.out.println("So tien ban nap vao khong hop le!");
         } else {
             balance += amount;
-            transitionList.add("Nap tien $" + String.format("%.2f", amount) + ". So du luc nay: $"
+            transitionList.add(new Transaction(Transaction.DEPOSIT, amount, balance));
+            System.out.println("Nap tien $" + String.format("%.2f", amount) + ". So du luc nay: $"
                     + String.format("%.2f", balance) + ".");
         }
     }
 
+    /**
+     * Withdraw.
+     */
     private void withdraw(double amount) {
         if (amount <= 0) {
             System.out.println("So tien ban rut ra khong hop le!");
@@ -27,21 +44,28 @@ public class Account {
             System.out.println("So tien ban rut vuot qua so du!");
         } else {
             balance -= amount;
-            transitionList.add("Rut tien $" + String.format("%.2f", amount) + ". So du luc nay: $"
+            transitionList.add(new Transaction(Transaction.WITHDRAW, amount, balance));
+            System.out.println("Rut tien $" + String.format("%.2f", amount) + ". So du luc nay: $"
                     + String.format("%.2f", balance) + ".");
         }
     }
 
+    /**
+     * Add transaction.
+     */
     public void addTransaction(double amount, String operation) {
-        if (operation.equals("deposit")) {
+        if (operation.equals(Transaction.DEPOSIT)) {
             deposit(amount);
-        } else if (operation.equals("withdraw")) {
+        } else if (operation.equals(Transaction.WITHDRAW)) {
             withdraw(amount);
         } else {
             System.out.println("Yeu cau khong hop le!");
         }
     }
 
+    /**
+     * Main.
+     */
     public static void main(String[] args) {
         Account acc = new Account();
         acc.addTransaction(2000.255, "deposit");
