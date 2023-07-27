210ab51da896ef6d9374a0da0ccf6cdf3651666a
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2023-07-27 21:29:14 +0700

    Add: Expression 2

---
+++ 15Expression2/Week8Task2.java
@@ -0,0 +1,72 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Week8Task2 {
+    public void nullPointerEx() throws NullPointerException {
+        String s = null;
+        s.length();
+    }
+
+    public void arrayIndexOutOfBoundsEx() throws ArrayIndexOutOfBoundsException {
+        String[] arr = new String[12];
+        arr[19] = "";
+    }
+
+    public void arithmeticEx() throws ArithmeticException {
+        int a = 69 / 0;
+    }
+
+    public void fileNotFoundEx() throws FileNotFoundException {
+        FileReader file = new FileReader("uetso1.hustga");
+    }
+
+    public void ioEx() throws IOException {
+        FileReader file = new FileReader("uetso1.hustga");
+    }
+
+    public String nullPointerExTest() {
+        try {
+            nullPointerEx();
+            return "Không có lỗi";
+        } catch (NullPointerException e) {
+            return "Lỗi Null Pointer";
+        }
+    }
+
+    public String arrayIndexOutOfBoundsExTest() {
+        try {
+            arrayIndexOutOfBoundsEx();
+            return "Không có lỗi";
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return "Lỗi Array Index Out of Bounds";
+        }
+    }
+
+    public String arithmeticExTest() {
+        try {
+            arithmeticEx();
+            return "Không có lỗi";
+        } catch (Exception e) {
+            return "Lỗi Arithmetic";
+        }
+    }
+
+    public String fileNotFoundExTest() {
+        try {
+            fileNotFoundEx();
+            return "Không có lỗi";
+        } catch (FileNotFoundException e) {
+            return "Lỗi File Not Found";
+        }
+    }
+
+    public String ioExTest() {
+        try {
+            ioEx();
+            return "Không có lỗi";
+        } catch (Exception e) {
+            return "Lỗi IO";
+        }
+    }
+}
